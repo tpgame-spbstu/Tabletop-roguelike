c0c04551e2fdaa372cc878f541ead2d7edaff454
maloxit <61049974+maloxit@users.noreply.github.com>
2022-04-12 00:50:43 +0300

    Заменил пробелы на табуляцию #39

--- Modification/player_deck.gd
+++ Modification/player_deck.gd
@@ -13,37 +13,37 @@ onready var card = load("res://Modification/card_2d.tscn")
 
 
 func _ready():
-    var _card
-    var nb_points = 10
-    var delta = (rad_to - rad_from) / nb_points
-    var size = get_viewport().get_size()
-    var center = Vector2(size.x / 2, size.y + sin(abs(rad_from)) * r_sin)
+	var _card
+	var nb_points = 10
+	var delta = (rad_to - rad_from) / nb_points
+	var size = get_viewport().get_size()
+	var center = Vector2(size.x / 2, size.y + sin(abs(rad_from)) * r_sin)
 
-    for i in range(nb_points + 1):
-        var phi = rad_from + i * delta
+	for i in range(nb_points + 1):
+		var phi = rad_from + i * delta
 
-        _card = card.instance()
-        add_child(_card)
-        _card.set_scale(card_scale)
-        _card.set_position(center + Vector2(cos(phi) * r_cos, sin(phi) * r_sin) - Vector2(0, _card.get_size().length() / 2))
-        # TAU / 4 = 90. Since the angles are counted from RIGHT (1, 0),
-        # +90 is needed to bring the top from left to up
-        _card.set_rotation(phi + TAU / 4)
+		_card = card.instance()
+		add_child(_card)
+		_card.set_scale(card_scale)
+		_card.set_position(center + Vector2(cos(phi) * r_cos, sin(phi) * r_sin) - Vector2(0, _card.get_size().length() / 2))
+		# TAU / 4 = 90. Since the angles are counted from RIGHT (1, 0),
+		# +90 is needed to bring the top from left to up
+		_card.set_rotation(phi + TAU / 4)
 
-        # connect the card's signals to handle them here
-        _card.connect("_mouse_entered", self, "_on_mouse_enter")
-        _card.connect("_mouse_exited", self, "_on_mouse_exit")
+		# connect the card's signals to handle them here
+		_card.connect("_mouse_entered", self, "_on_mouse_enter")
+		_card.connect("_mouse_exited", self, "_on_mouse_exit")
 
 
 """
-    FIXME: for some reason the cards are changed faster when moving through them from left to right
+	FIXME: for some reason the cards are changed faster when moving through them from left to right
 """
 func _on_mouse_enter(card):
-    # display the hovered card on top of others
-    card.set_z_index(1)
+	# display the hovered card on top of others
+	card.set_z_index(1)
 
 
 func _on_mouse_exit(card):
-    # the cards are dr_coswn in their order in the scene tree, hence
-    # setting the z_index to 0 doesn't breaks the cards' order
-    card.set_z_index(0)
+	# the cards are dr_coswn in their order in the scene tree, hence
+	# setting the z_index to 0 doesn't breaks the cards' order
+	card.set_z_index(0)
