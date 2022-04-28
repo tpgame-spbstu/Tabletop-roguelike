211c074b2717f74a450192eab9ea24efcc94f2f2
lyphrowny <strukov.as@edu.spbstu.ru>
2022-04-29 01:57:03 +0300

    Перешел с использования card_3d на proxy в руке #97

--- Modification/hand.gd
+++ Modification/hand.gd
@@ -6,17 +6,21 @@ signal mouse_exited(card)
 
 # stores the cards
 var _cards: Array
-# stores currently hovered card
+# stores currently hovered proxy
 var _hovered
-# set for every card, ray is also checking only this layer
-# done to avoid ray picking cards, that are not in the hand
+# set for every proxy, ray is also checking only this layer
+# done to avoid ray picking proxy, that are not in the hand
 var _collision_layer_bit: int = 2
-onready var card_inst = load("res://Modification/card_3d.tscn")
-onready var utils = load("res://Modification/utils.gd").new()
+onready var _proxy = preload("res://Modification/proxy.tscn")
+var _proxies: Array = []
+onready var card_inst = preload("res://cart_test.tscn")
+onready var utils = preload("res://Modification/utils.gd").new()
 # TODO: substitute, change it with the actual value
 const OFFSET := Vector3(0, 0.5, -2.5)
 
 
+# for running as a standalone scene
+# one can safely remove the whole method
 func _ready():
 	var cards = [
 		card_inst.instance(),
@@ -42,22 +46,22 @@ func _notification(what):
 				emit_signal("mouse_exited", _hovered)
 
 
-## checks whether the hovered card and the current mouse position are on the different
+## checks whether the hovered proxy and the current mouse position are on the different
 ## sides of the line
 ##
-## @desc: in order to make the transition from hovered card to the one under it, an intersecting
+## @desc: in order to make the transition from hovered proxy to the one under it, an intersecting
 ##        polygon is created. It is split in two halves (almost) using `least square regression`.
-##        if the hovered card is on one side of the regression line while the mouse position is on the other,
-##        then one needs to make the card, on which the mouse is on, hovered,
+##        if the hovered proxy is on one side of the regression line while the mouse position is on the other,
+##        then one needs to make the proxy, on which the mouse is on, hovered,
 ##        and the one hovered at the moment unhovered
 ##
 ## :line_coeffs: coeffs of the line
-## :card_pos: position of the hovered card
+## :proxy_pos: position of the hovered card
 ## :mouse_pos: position of the mouse
 ##
 ## :return: true, if on the opposite sides of the line, false otherwise
-func _is_hovering_new_card(line_coeffs: Vector2, card_pos: Vector2, mouse_pos: Vector2):
-	return utils.get_line_sign(line_coeffs, card_pos) * utils.get_line_sign(line_coeffs, mouse_pos) < 0
+func _is_hovering_new_proxy(line_coeffs: Vector2, proxy_pos: Vector2, mouse_pos: Vector2):
+	return utils.get_line_sign(line_coeffs, proxy_pos) * utils.get_line_sign(line_coeffs, mouse_pos) < 0
 
 
 ## actions taken on mouse hovering the cards
@@ -93,45 +97,61 @@ func _mouse_hovering(event):
 	var ray_to = ray_from + cam.project_ray_normal(mouse_pos) * RAY_LENGTH
 	var space_state = get_world().direct_space_state
 
-	# get the closest (highest on Y-axis) card under the mouse
-	var first_card = space_state.intersect_ray(ray_from, ray_to, [], _get_collision_layer())
-	if first_card:
-		first_card = first_card["collider"]
+	# get the closest (highest on Y-axis) proxy under the mouse
+	# godot doesn't support named function arguments, hence the comments
+	var first_proxy = space_state.intersect_ray(
+		ray_from,  # from
+		ray_to,  # to
+		[],  # exclude
+		_get_collision_layer(),  # collision layer of the ray
+		false,  # collide with bodies
+		true  # collide with areas (proxy is an area)
+	)
+	if first_proxy:
+		first_proxy = first_proxy["collider"]
 		# case (2)
 		# if not taking into account the tween, recursion between cases 2 and 3
 		if not _hovered:
-			if not first_card.get_hower_tween().is_active() or Geometry.is_point_in_polygon(Vector2(ray_to.x, ray_to.z), first_card._get_2dpoints()):
-				emit_signal("mouse_entered", first_card)
+			if not first_proxy.get_hower_tween().is_active() or Geometry.is_point_in_polygon(Vector2(ray_to.x, ray_to.z), first_proxy._get_2dpoints()):
+				emit_signal("mouse_entered", first_proxy)
 		else:
 			# case (6)
-			if first_card != _hovered:
+			if first_proxy != _hovered:
 				emit_signal("mouse_exited", _hovered)
-				emit_signal("mouse_entered", first_card)
+				emit_signal("mouse_entered", first_proxy)
 				return
-			# get the card under the first card, if any
-			# [_hovered] means to ignore the hovered card during raycasting
-			var second_card = space_state.intersect_ray(ray_from, ray_to, [_hovered], _get_collision_layer())
-			if second_card:
-				second_card = second_card["collider"]
-				# get the polygons of the cards (array of their vertexes)
-				var poly1 = first_card._get_2dpoints()
-				var poly2 = second_card._get_2dpoints()
+			# get the proxy under the first proxy, if any
+			# [_hovered] means to ignore the hovered proxy during the ray casting
+			var second_proxy = space_state.intersect_ray(
+				ray_from,  # from
+				ray_to,  # to
+				[_hovered],  # excluded
+				_get_collision_layer(),  # collision layer of the ray
+				false,  # collide with bodies
+				true  # collides with areas
+			)
+			if second_proxy:
+				second_proxy = second_proxy["collider"]
+				# get the polygons of the proxies (array of their vertexes)
+				var poly1 = first_proxy._get_2dpoints()
+				var poly2 = second_proxy._get_2dpoints()
 				var inter = Geometry.intersect_polygons_2d(poly1, poly2)
-				# check whether the two cards intersect
+				# check whether the two proxies intersect
 				if inter:
-					# get the centerline of the intersection area
+					# get the center-line of the intersection area
 					var line_coeffs = utils.least_squares_regression(inter[0])
-					var mouse_local_pos = Vector2(ray_to.x, ray_to.z)
-					var first_card_trans = first_card.get_translation()
-					var first_card_pos = Vector2(first_card_trans.x, first_card_trans.z)
+					var first_proxy_trans = first_proxy.get_translation()
+					var mouse_local_pos = cam.project_position(mouse_pos, first_proxy_trans.y)
+					mouse_local_pos = Vector2(mouse_local_pos.x, mouse_local_pos.z)
+					var first_proxy_pos = Vector2(first_proxy_trans.x, first_proxy_trans.z)
 
-					# check whether the hovered card and the mouse are on different sides of the line
-					if _is_hovering_new_card(line_coeffs, first_card_pos, mouse_local_pos):
+					# check whether the hovered proxy and the mouse are on different sides of the line
+					if _is_hovering_new_proxy(line_coeffs, first_proxy_pos, mouse_local_pos):
 						# case (5)
-						emit_signal("mouse_exited", first_card)
-						emit_signal("mouse_entered", second_card)
+						emit_signal("mouse_exited", first_proxy)
+						emit_signal("mouse_entered", second_proxy)
 	# case (3)
-	elif _hovered and not first_card:
+	elif _hovered and not first_proxy:
 		emit_signal("mouse_exited", _hovered)
 
 
@@ -140,11 +160,16 @@ func _input(event):
 		_mouse_hovering(event)
 
 
-func initialize(cards: Array):
-	add_cards(cards)
-	# the card is passed, so connect only oneself
-	connect("mouse_entered", self, "_on_mouse_entered")
-	connect("mouse_exited", self, "_on_mouse_exited")
+func _on_mouse_entered(proxy):
+	_hovered = proxy
+	var tween = proxy.get_hower_tween()
+	tween.on_enter(OFFSET)
+
+
+func _on_mouse_exited(proxy):
+	_hovered = null
+	var tween = proxy.get_hower_tween()
+	tween.on_exit()
 
 
 func _get_collision_layer():
@@ -160,15 +185,15 @@ func _add_card(card):
 	# make self the parent of the child
 	add_child(card)
 
-	card.set_collision_layer_bit(_collision_layer_bit, true)
+	proxy.set_collision_layer_bit(_collision_layer_bit, true)
 	_cards.push_back(card)
+	_proxies.push_back(proxy)
 
 
 func add_card(card) -> Array:
 	_add_card(card)
 	draw_cards()
-	# _remove_card(card)
-	return utils.get_placement_coeffs(_cards, true)
+	return utils.get_placement_coeffs(_proxies, true)
 
 
 func add_cards(cards: Array):
@@ -195,22 +220,10 @@ func draw_cards():
 	var coeffs = utils.get_placement_coeffs(_cards)
 	for i in coeffs.size():
 		# place the center of the cards on the line
-		trans = _cards[i].get_transform()
+		trans = _proxies[i].get_transform()
 		trans.origin = coeffs[i][0]
 		# rotate the card
 		trans.basis = Basis.IDENTITY.rotated(Vector3.UP, -coeffs[i][1])
-		_cards[i].set_transform(trans)
+		_proxies[i].set_transform(trans)
 		# set the original transform (position + rotation)
-		_cards[i].set_orig_trans(trans)
-
-
-func _on_mouse_entered(card):
-	_hovered = card
-	var tween = card.get_hower_tween()
-	tween.on_enter(OFFSET)
-
-
-func _on_mouse_exited(card):
-	_hovered = null
-	var tween = card.get_hower_tween()
-	tween.on_exit()
+		_proxies[i].set_orig_trans(trans)
