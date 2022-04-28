498ecc6e98ad12a1ea54787d2a37249451ff62e0
lyphrowny <strukov.as@edu.spbstu.ru>
2022-04-29 01:40:33 +0300

    Добавил сцену тестов добавления \ удаления карт из руки #97

--- Modification/hand_test.gd
+++ Modification/hand_test.gd
@@ -1,15 +1,18 @@
 extends Spatial
 
-onready var card_inst = load("res://Modification/card_3d.tscn")
+onready var card_inst = load("res://cart_test.tscn")
 onready var hand = load("res://Modification/hand.tscn")
 
 
 func _ready():
-	var c = card_inst.instance()
-	c.translate(Vector3(-7, 0, -5))
-	c.set_orig_trans(c.get_transform())
-	add_child(c)
 	var h = hand.instance()
 	add_child(h)
-	c.connect("doubleclick", h, "add_card")
+
+	var c = card_inst.instance()
+	c.translate(Vector3(-7, 0, -5))
+	add_child(c)
+	# one can replace these with direct func calls
+	c.connect("left", h, "add_card")
+	c.connect("right", h, "remove_card")
+
 	$Camera.make_current()
