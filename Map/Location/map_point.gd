b35a60d043357b37de5ae053d6ab13e4a3dd0906
lyphrowny <strukov.as@edu.spbstu.ru>
2022-04-29 21:32:44 +0300

    Добавил визуальное сопровожнение наведению на точку локации на карте #146

--- Map/Location/map_point.gd
+++ Map/Location/map_point.gd
@@ -7,6 +7,9 @@ signal map_point_click(map_point)
 func initialize(map, map_point_config):
 	self.map_point_config = map_point_config
 	connect("map_point_click", map, "_on_map_point_click")
+	# connecting the hovering signals
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
 	_set_textures(map_point_config.get_textures())
 
 
@@ -30,3 +33,11 @@ func _on_Point_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.get_button_mask() == BUTTON_LEFT:
 			emit_signal("map_point_click", self)
+
+
+func _on_mouse_entered():
+	$on_hover_mesh.show()
+
+
+func _on_mouse_exited():
+	$on_hover_mesh.hide()
