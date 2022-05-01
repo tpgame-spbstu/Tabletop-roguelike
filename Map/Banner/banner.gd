ece7b28ab69979d01e2045cdfbbad7cce7f0b6b5
lyphrowny <strukov.as@edu.spbstu.ru>
2022-05-02 01:26:06 +0300

    Добавил анимацию появления флагштока; вынес vars в editor; исправил delta у exports #146

--- Map/Banner/banner.gd
+++ Map/Banner/banner.gd
@@ -3,24 +3,40 @@ extends Spatial
 
 onready var _pole: MeshInstance = $pole
 onready var _flag: MeshInstance = $flag
+# @warn: if changing the texture, set the correct
+#        translation! (it's shifted for the current one)
+onready var _inc_cirle: Sprite3D = $incantation_circle
 onready var _tween: Tween = Tween.new()
-const top = "top"
-const bottom = "bottom"
-const height = "height"
 # ratio of the height of flag to the height of the flag pole
-export(float, 0.01, 1, 0.2) var hfl_to_hpole = 0.2
+export(float, 0.01, 1, 0.01) var hfl_to_hpole = 0.2
 # percentage of flag pole height to skip before the top of the flag
-export(float, 0.01, 0.49, 0.05) var margin_from_top = 0.05
+export(float, 0.01, 0.49, 0.01) var margin_from_top = 0.05
 # percentage of flag pole height to skip before the bottom of the flag
-export(float, 0.01, 0.49, 0.05) var margin_from_bottom = 0.05
-# the animation will be player this number of seconds
-export(int, 0.1, 4, 2) var anim_dur = 2
+export(float, 0.01, 0.49, 0.01) var margin_from_bottom = 0.05
+# the flag raising will be played this number of seconds
+export(float, 0.1, 4, 0.1) var flag_raising_dur = 2
+# the incantation circle will fully appear in this num of secs
+export(float, 0.1, 4, 0.1) var inc_appearance_dur = 2
+# the desired scaling will be reached in this num of secs
+export(float, 0.1, 4, 0.1) var inc_scaling_dur = 2
+# the desired scaling of the incantation cirle
+export(float, 0.1, 1, 0.1) var inc_scale_coeff = 0.5
+# the pole will take its place in this num of secs
+export(float, 0.1, 4, 0.1) var pole_appearance_dur = 2
+# the incantation cirle will fully disappear in this num of secs
+export(float, 0.1, 4, 0.1) var inc_disappearance_dur = 2
+# the incantation cirle will be descaled in this num of secs
+export(float, 0.1, 4, 0.1) var inc_descaling_dur = 2
+# whether to descale the incantation circle (it will be made invisible anyway)
+export(bool) var descaling = true
 
 
 func _ready():
+	# hide all of them
+	# their appearance is controlled by tweens
+	_pole.hide()
 	_flag.hide()
-	add_child(_tween)
-	_tween.connect("tween_all_completed", self, "_on_tween_completed")
+	_inc_cirle.hide()
 
 
 func get_pole_size():
@@ -30,16 +46,6 @@ func get_pole_size():
 		cyl_mesh.get_height(),
 		cyl_mesh.get_bottom_radius()
 	)
-	# return {
-	# 	top: cyl_mesh.get_top_radius(),
-	# 	bottom: cyl_mesh.get_bottom_radius(),
-	# 	height: cyl_mesh.get_height()
-	# }
-
-
-# func get_centered_trans():
-# 	var pole_size = _get_pole_size()
-# 	return Vector3(pole_size[top] / 2, pole_size[height] / 2, pole_size[top] / 2)
 
 
 func get_flag_size():
@@ -47,8 +53,47 @@ func get_flag_size():
 	return plane_mesh.get_size()
 
 
+## let the pole make its appearance on the scene
+func _show_stick():
+	# need to mentally prepare both the `_inc_cirle` and the `_pole`
+	# before appearing on the scene. If not, they will appear as is
+	# firtly, then the tween will start: set the fully shown `_inc_cirle_`
+	# to being invisible with "modulate:a" -> disappear shortly after appearing, BAD
+	_inc_cirle.modulate.a = 0
+	_inc_cirle.show()
+	var pole_offset = get_pole_size().y / 2
+	_pole.translation.y -= pole_offset
+	_pole.show()
+
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(_inc_cirle, "modulate:a", 0, 1,
+		inc_appearance_dur, Tween.TRANS_QUART, Tween.EASE_OUT)
+	tween.interpolate_property(_inc_cirle, "scale", Vector3.ZERO, Vector3.ONE * inc_scale_coeff,
+		inc_scaling_dur, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tween.interpolate_property(_pole, "translation:y", -pole_offset, pole_offset,
+		pole_appearance_dur, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	tween.start()
+	# wait for the tween to end for the flag not to appear before the pole is at its final position
+	yield(tween, "tween_all_completed")
+
+	# immediately after start the disappearing of the incantation circle. will be animated at the 
+	# same time the flag is raising
+	tween.interpolate_property(_inc_cirle, "modulate:a", 1, 0,
+		inc_disappearance_dur, Tween.TRANS_SINE, Tween.EASE_IN)
+	if descaling:
+		tween.interpolate_property(_inc_cirle, "scale", _inc_cirle.get_scale(), Vector3.ZERO,
+			inc_descaling_dur, Tween.TRANS_SINE, Tween.EASE_IN)
+	tween.start()
+	# thank you for your service, sir!
+	tween.connect("tween_all_completed", self, "_on_tween_completed", [tween])
+
+
 ## shows the flag and plays its animation (movement from the bottom of the pole to its top)
 func set_sail():
+	# no flag without a stick, huh?
+	yield(_show_stick(), "completed")
+
 	var pole_size = get_pole_size()
 	var fl_size = get_flag_size()
 	# set the scale so that the `hfl_to_hstk` ratio is true
@@ -68,13 +113,16 @@ func set_sail():
 	var final_y = pole_trans.y + pole_size.y * (1 - margin_from_top) / 2 - fl_size.y / 2
 
 	_flag.show()
+	var tween = Tween.new()
+	add_child(tween)
 	# just for chuckles :) for normal use any of the commented below is recommended
-	_tween.interpolate_property(_flag, "translation:y", init_y, final_y, anim_dur, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	tween.interpolate_property(_flag, "translation:y", init_y, final_y, flag_raising_dur, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
 	# _tween.interpolate_property(_flag, "translation:y", init_y, final_y, anim_dur, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	# _tween.interpolate_property(_flag, "translation:y", init_y, final_y, anim_dur, Tween.TRANS_CIRC, Tween.EASE_OUT)
-	_tween.start()
+	tween.start()
+	tween.connect("tween_all_completed", self, "_on_tween_completed", [tween])
 
 
-func _on_tween_completed():
-	remove_child(_tween)
-	_tween.queue_free()
+func _on_tween_completed(tween):
+	remove_child(tween)
+	tween.queue_free()
