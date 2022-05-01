8f584bb77d6cbbc9433b188223a71ed7397ce7be
lyphrowny <strukov.as@edu.spbstu.ru>
2022-05-01 03:47:53 +0300

    Сделал настройку viewport'а, отображающего label #139

--- Fight/damage_anim/damage_float_main.gd
+++ Fight/damage_anim/damage_float_main.gd
@@ -1,12 +1,11 @@
 extends Sprite3D
 
-onready var _viewport: Viewport = $Viewport
-onready var _dmg: Label = $Viewport/dmg
+onready var _viewport = $Viewport
+onready var _dmg = $Viewport/dmg
+
 
 func _ready():
-	_viewport.set_size(Vector2(100, 100))
-	print("before ", _dmg.get_size())
-	_dmg.set_msg("Yo!")
-	print("after: ", _dmg.get_size())
+	_dmg.set_msg("3")
 
 	set_texture(_viewport.get_texture())
+	set_billboard_mode(SpatialMaterial.BILLBOARD_ENABLED)
--- Fight/damage_anim/dmg.gd
+++ Fight/damage_anim/dmg.gd
@@ -1,13 +1,18 @@
 extends Label
 
 
+signal set_text
+
+
 export(DynamicFont) var font = DynamicFont.new()
 export(DynamicFontData) var font_data = load("res://fonts/Ubuntu-B.ttf")
-export(int, 4, 100, 56) var font_size = 56
+export(int, 4, 100, 1) var font_size = 56
 export(int, 0, 10, 1) var outline_size = 1
 export(Color, RGBA) var outline_color = Color(0, 0, 0, 1)
 export(bool) var use_mipmaps = true
 export(bool) var use_filter = true
+
+export(int, 1, 4) var max_chars = 4
 
 
 func _ready():
@@ -15,9 +20,25 @@ func _ready():
 
 
 func set_msg(msg):
-	set_text(str(msg))
+	msg = str(msg)
+	assert(msg.length() <= max_chars)
+	set_text(msg)
 	# wait to update the size
 	yield(get_tree(), "idle_frame")
+	# FIXME does nothing
+	set_size(Vector2.ONE * max(get_size().x, get_size().y))
+	# the size might've been changed with the new text
+	# tell everyone 'bout it
+	emit_signal("set_text")
+
+
+# doesn't work, like at all
+# let it be as a monument
+#
+# TODO: either find solution to draw the
+# the label normally, or remove, since it does nothing
+func get_recommened_vp_size() -> Vector2:
+	return Vector2.ONE * max(get_size().x, max_chars * font_size) * 8
 
 
 func _configure():
@@ -31,3 +52,5 @@ func _configure():
 	font.use_mipmaps = use_mipmaps
 	font.use_filter = use_filter
 	set("custom_fonts/font", font)
+
+	set_visible_characters(max_chars)
