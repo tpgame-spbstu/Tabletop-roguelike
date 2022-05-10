586ae80e6aedbac27be85334403e79f8f98fd25d
Enoras <raul.akhm@gmail.com>
2022-05-10 14:12:27 +0300

    Создал отдельную сцену для ноды гема #135

--- Fight/resources_status.gd
+++ Fight/resources_status.gd
@@ -15,79 +15,73 @@ var energy_start_pos = Vector3(-2.594,0,-1.04)
 var bones_start_pos = Vector3(-2.594,0,-1.377)
 var blood_start_pos = Vector3(-2.594,0,-1.714)
 
+var t_gems = preload("res://Models/Scene/gems.tscn").instance()
 
-var t_mesh = preload("res://Models/Scene/gems_mesh.tres")
 
-
-func energy_null() :
+func _energy_null() :
 	var energy_child = $energy_status.get_children()
 	for i in energy_child:
 		i.get_active_material(0).albedo_color = epty_color
 	
 	
-func bones_null() :
+func _bones_null() :
 	var bones_child = $bones_status.get_children()
 	for i in bones_child:
 		i.get_active_material(0).albedo_color = epty_color
-		
 	
-func blood_null() :
+	
+func _blood_null() :
 	var blood_child = $blood_status.get_children()
 	for i in blood_child:
 		i.get_active_material(0).albedo_color = epty_color
-		
-		
-func add_energy_to_scene():
+	
+	
+func create_gem():
+	var temp =  load("res://Models/Scene/gems.tscn").instance()
+	var t_material = SpatialMaterial.new()
+	temp.set_surface_material(0,t_material)
+	return temp
+	
+	
+func _add_energy_to_scene():
 	for i in range(energy_count):
-		var temp = MeshInstance.new()
-		temp.mesh = t_mesh
-		var t_material = SpatialMaterial.new()
-		temp.set_surface_material(0,t_material)
-		temp.scale = gems_scale
+		var temp = create_gem()
 		$energy_status.add_child(temp)
 		temp.translation = energy_start_pos + space * i
 		
-func add_bones_to_scene():
+		
+func _add_bones_to_scene():
 	for i in range(bones_count):
-		var temp = MeshInstance.new()
-		temp.mesh = t_mesh
-		var t_material = SpatialMaterial.new()
-		temp.set_surface_material(0,t_material)
-		temp.scale = gems_scale
+		var temp = create_gem()
 		$bones_status.add_child(temp)
 		temp.translation = bones_start_pos + space * i
 
-func add_blood_to_scene():
+
+func _add_blood_to_scene():
 	for i in range(blood_count):
-		var temp = MeshInstance.new()
-		temp.mesh = t_mesh
-		var t_material = SpatialMaterial.new()
-		temp.set_surface_material(0,t_material)
-		temp.scale = gems_scale
+		var temp = create_gem()
 		$blood_status.add_child(temp)
 		temp.translation = blood_start_pos + space * i
 	
+	
 func _ready():
-	add_energy_to_scene()
-	add_bones_to_scene()
-	add_blood_to_scene()
-	bones_null()
-	blood_null()
+	_add_energy_to_scene()
+	_add_bones_to_scene()
+	_add_blood_to_scene()
+	_bones_null()
+	_blood_null()
 	
 	
-
 func _on_human_player_state_energy_changed(energy):
-	energy_null()
+	_energy_null()
 	var energy_child = $energy_status.get_children()
 	for i in range(energy):
 		energy_child[i].get_active_material(0).albedo_color = energy_full_color
 
 
-
 func _on_human_player_state_bones_changed(bones):
-	bones_null()
+	_bones_null()
 	var bones_child = $bones_status.get_children()
 	for i in range(bones):
 		bones_child[i].get_active_material(0).albedo_color = bones_full_color
-	
 	
