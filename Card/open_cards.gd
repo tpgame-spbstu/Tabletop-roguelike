5dddd8a65e19cb54aa072e1b2bab868d3f35e18b
maloxit <61049974+maloxit@users.noreply.github.com>
2022-05-12 21:26:42 +0300

    Добавил настроек и функций open_cards, перенёс в другую директорию #160

--- Card/open_cards.gd
+++ Card/open_cards.gd
@@ -2,15 +2,31 @@ extends Spatial
 
 
 var _cards = []
-var margin_left = 0.4
-var margin_right = 0.4
+export(float) var margin_left = 0.4
+export(float) var margin_right = 0.4
+export(float) var min_scale_size = 0.8
+export(float) var max_scale_size = 1.5
+export(float) var max_dist_between = 0.35
 var _last_hovered_index = 0
 var _is_hovered = false
+var _is_input_allowed := true
 
 
 signal hovered_card_changed(card)
 signal card_left_click(card)
 signal card_right_click(card)
+
+
+func is_input_allowed():
+	return _is_input_allowed
+
+
+func allow_input():
+	_is_input_allowed = true
+
+
+func disallow_input():
+	_is_input_allowed = false
 
 
 func get_cards_count():
@@ -21,6 +37,13 @@ func is_hovered():
 	return _is_hovered
 
 
+func get_last_hovered_card():
+	if _last_hovered_index in range(_cards.size()):
+		return _cards[_last_hovered_index]
+	else:
+		return null
+
+
 func add_card(card, position = 'front'):
 	assert(position == 'front' or position == 'back')
 	if position == 'front':
@@ -29,6 +52,7 @@ func add_card(card, position = 'front'):
 		_cards.push_back(card)
 	add_child(card)
 	card.transform = Transform()
+	
 	reset_cards_positions()
 
 
@@ -54,6 +78,9 @@ func remove_card(card):
 
 
 func reset_cards_positions():
+	var correct_size = _get_correct_scale_size()
+	if $lin_hover_scale.scale.x != correct_size:
+		$lin_hover_scale.scale.x = correct_size
 	for i in _last_hovered_index:
 		if _cards[i]:
 			_cards[i].transform.origin = Vector3(_get_pos_from_index(i), 0, 0)
@@ -71,6 +98,17 @@ func reset_cards_positions():
 		if _cards[i]:
 			_cards[i].transform.origin = Vector3(_get_pos_from_index(i), 0, 0)
 			_cards[i].rotation.z = -0.1
+
+
+func _get_correct_scale_size():
+	var size = margin_left + margin_right
+	if _cards.size() > 0:
+		size += (_cards.size() - 1) * max_dist_between
+	if size < max_scale_size:
+		if size < min_scale_size:
+			return min_scale_size
+		return size
+	return max_scale_size
 
 
 func _get_pos_from_index(index):
@@ -97,13 +135,6 @@ func _get_index_from_value(value):
 	return index
 
 
-func get_last_hovered_card():
-	if _last_hovered_index in range(_cards.size()):
-		return _cards[_last_hovered_index]
-	else:
-		return null
-
-
 func _set_hovered_index(index):
 	_last_hovered_index = index
 	reset_cards_positions()
@@ -121,20 +152,28 @@ func _process_new_hover_value(value):
 
 
 func _on_lin_hover_scale_mouse_motion(value):
+	if not is_input_allowed():
+		return
 	_process_new_hover_value(value)
 
 
 func _on_lin_hover_scale_mouse_exited():
+	if not is_input_allowed():
+		return
 	_is_hovered = false
 	reset_cards_positions()
 	emit_signal("hovered_card_changed", null)
 
 
 func _on_lin_hover_scale_left_click(value):
+	if not is_input_allowed():
+		return
 	_process_new_hover_value(value)
 	emit_signal("card_left_click", get_last_hovered_card())
 
 
 func _on_lin_hover_scale_right_click(value):
+	if not is_input_allowed():
+		return
 	_process_new_hover_value(value)
 	emit_signal("card_right_click", get_last_hovered_card())
