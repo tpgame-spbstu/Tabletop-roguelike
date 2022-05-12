b8fcee4a2468633142d1364260dbc0b468994cc2
maloxit <61049974+maloxit@users.noreply.github.com>
2022-05-12 21:46:05 +0300

    Исправил выход текста проигрыша за рамки #203

--- Fight/fight_location_ui.gd
+++ Fight/fight_location_ui.gd
@@ -121,7 +121,7 @@ func _on_loop_mouse_exited():
 
 func show_final_status(message):
 	$Final_status.show()
-	$Final_status/RichTextLabel.text = message
+	$Final_status/RichTextLabel.bbcode_text = message
 	$Final_status/return_to_map.show()
 
 
@@ -142,11 +142,11 @@ func _on_final_status_return_to_map_pressed():
 
 
 func _on_fight_state_player_1_win_enter():
-	show_final_status("Победа")
+	show_final_status("[center]Победа[/center]")
 
 
 func _on_fight_state_player_2_win_enter():
-	show_final_status("Проигрыш")
+	show_final_status("[center]Проигрыш[/center]")
 
 
 func _on_button_exit_to_main_menu_pressed():
