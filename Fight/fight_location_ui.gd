8497e17e266f37359264c58d5edf0400b55abc5f
maloxit <61049974+maloxit@users.noreply.github.com>
2022-05-23 16:19:51 +0300

    Вернул отображение ui, удалил ненужные элементы #134

--- Fight/fight_location_ui.gd
+++ Fight/fight_location_ui.gd
@@ -21,23 +21,11 @@ func _on_fight_state_loop_number_changed(loop_number):
 	$loop.text = loop_number as String
 
 
-func _on_fight_state_player_1_health_changed(player_1_health):
-	$ColorRect/HP1.text = player_1_health as String
-
-
-func _on_fight_state_player_2_health_changed(player_2_health):
-	$ColorRect/HP2.text = player_2_health as String
-
-
 func _on_fight_state_player_1_draw_cards_enter():
 	$Status1.margin_left = -120
 	$Status1.text = "Взятие карты"
 	$Status1.show()
 	$Status2.hide()
-	$ColorRect/Bones.show()
-	$ColorRect/Energy.show()
-	$ColorRect/card.text = "2"
-	$ColorRect/card.show()
 
 
 func _on_fight_state_player_1_attack_enter():
@@ -53,62 +41,6 @@ func _on_fight_state_player_1_place_and_move_enter():
 func _on_fight_state_player_2_draw_cards_enter():
 	$Status2.show()
 	$Status1.hide()
-	$ColorRect/Bones.hide()
-	$ColorRect/Energy.hide()
-	$ColorRect/card.hide()
-
-
-func _on_human_player_state_bones_changed(bones):
-	$ColorRect/Bones.text = bones as String
-
-
-func _on_human_player_state_energy_changed(energy):
-	$ColorRect/Energy.text = energy as String
-
-
-func _on_human_player_state_extra_draws_count_changed(extra_draws_count):
-	$ColorRect/card.text = extra_draws_count as String
-
-
-
-func _on_HP2_mouse_entered():
-	$ColorRect/HP_bot_label.show()
-
-
-func _on_HP1_mouse_entered():
-	$ColorRect/HP_player_label.show()
-
-
-func _on_HP2_mouse_exited():
-	$ColorRect/HP_bot_label.hide()
-
-
-func _on_Energy_mouse_entered():
-	$ColorRect/Energy_label.show()
-
-
-func _on_Energy_mouse_exited():
-	$ColorRect/Energy_label.hide()
-
-
-func _on_HP1_mouse_exited():
-	$ColorRect/HP_player_label.hide()
-
-
-func _on_Bones_mouse_entered():
-	$ColorRect/Bones_label.show()
-
-
-func _on_Bones_mouse_exited():
-	$ColorRect/Bones_label.hide()
-
-
-func _on_card_mouse_entered():
-	$ColorRect/Card_label.show()
-
-
-func _on_card_mouse_exited():
-	$ColorRect/Card_label.hide()
 
 
 func _on_loop_mouse_entered():
